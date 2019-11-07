015271431cf5f0d7beabb93a58d56de7c7794a29
Yanis BOUDRAHEM <yanisboudrahem00@gmail.com>
2019-11-07 16:16:05 +0100

    commit

---
+++ ft_strjoin.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*joined;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	if ((joined = (char*)malloc(sizeof(char)
+			* (ft_strlen(s1) + ft_strlen(s2) + 1))) == NULL)
+		return (NULL);
+	joined = ft_strcpy(joined, s1);
+	return (ft_strcat(joined, s2));
+}
---
+++ ft_substr.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	char			*sub;
+
+	if (s == NULL)
+		return (NULL);
+	if ((sub = (char*)malloc(sizeof(char) * (len + 1))) == NULL)
+		return (NULL);
+	i = 0;
+	if (!(start > ft_strlen(s)))
+	{
+		while (i < len && s[start + i])
+		{
+			sub[i] = s[start + i];
+			i++;
+		}
+	}
+	sub[i] = '\0';
+	return (sub);
+}
