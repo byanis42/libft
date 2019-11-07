5295f9750a48d13bd7b64e0de38892508b31a1bd
Yanis BOUDRAHEM <yanisboudrahem00@gmail.com>
2019-11-07 16:17:32 +0100

    commit

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -10,6 +10,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if ((joined = (char*)malloc(sizeof(char)
 			* (ft_strlen(s1) + ft_strlen(s2) + 1))) == NULL)
 		return (NULL);
-	joined = ft_strcpy(joined, s1);
-	return (ft_strcat(joined, s2));
+	joined = ft_strcpy(joined, s1); //need strcpy verifier avec sujet libft si possible
+	return (ft_strcat(joined, s2)); //need strcat
 }
