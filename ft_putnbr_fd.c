fbabcf05ef30dca783d90298c3c2f7ed29794024
Yanis BOUDRAHEM <yanisboudrahem00@gmail.com>
2019-11-15 11:40:56 +0100

    commit

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	nb;
+
+	if (n < 0)
+	{
+		nb = -n;
+		ft_putchar_fd('-', fd);
+	}
+	else
+		nb = n;
+	if (nb >= 10)
+	{
+		ft_putnbr_fd((nb / 10), fd);
+		ft_putchar_fd(((nb % 10) + '0'), fd);
+	}
+	else
+	{
+		if (n > 0)
+			ft_putchar_fd(('0' + n), fd);
+		else
+			ft_putchar_fd(('0' - n), fd);
+	}
+}
